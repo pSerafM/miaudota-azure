55a8baf6cdf50e1e033f405eb9df0dea2f3ea500
Pedro Serafim <166882606+pSerafM@users.noreply.github.com>
2026-09-01 14:21:23 -0300

    melhorias visuais (fiz na mão, aceito sugestões)

--- js/index.js
+++ js/index.js
@@ -14,19 +14,19 @@ async function carregarPets() {
         pets.forEach(pet => {
             const card = document.createElement('div');
             card.className = 'card-pet';
-            card.style.border = "1px solid #ccc";
-            card.style.padding = "15px";
-            card.style.margin = "10px 0";
-            card.style.borderRadius = "8px";
 
             // Cria o HTML do card. Repare que estamos usando o _id que vem do MongoDB
             card.innerHTML = `
                 <h3>${pet.nome}</h3>
+                <br>
                 <p><strong>Espécie:</strong> ${pet.especie} | <strong>Raça:</strong> ${pet.raca}</p>
-                <p><strong>Idade:</strong> ${pet.idade} anos | <strong>Sexo:</strong> ${pet.sexo} | <strong>Porte:</strong> ${pet.porte}</p>
-                <p>${pet.descricao}</p>
-                <button onclick="editar('${pet._id}', '${pet.nome}')" style="background:#f0ad4e; color:white; padding:5px; border:none; cursor:pointer; margin-right: 5px;">Editar Nome</button>
-                <button onclick="excluir('${pet._id}')" style="background:#d9534f; color:white; padding:5px; border:none; cursor:pointer;">Excluir</button>
+                <p><strong>Idade:</strong> ${pet.idade} anos | <strong>Sexo:</strong> ${pet.sexo} </p>
+                <p><strong>Porte:</strong> ${pet.porte}</p>
+                <br>
+                <p id="descricao"> "${pet.descricao}" </p>
+                <br>
+                <button onclick="editar('${pet._id}', '${pet.nome}')" class="botao editar">Editar Nome</button>
+                <button onclick="excluir('${pet._id}')" class="botao excluir">Excluir</button>
             `;
 
             container.appendChild(card);
